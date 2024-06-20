2dd7ad984d568b164810ce525c0b80a97ebaf4ba
HeoManBo <iovesssr__@naver.com>
2024-06-20 18:17:36 +0900

    refactor : 스케쥴러 메소드 servie로 이동

--- src/main/java/com/god/life/service/FcmAlarmService.java
+++ src/main/java/com/god/life/service/FcmAlarmService.java
@@ -8,18 +8,23 @@ import com.god.life.error.BadRequestException;
 import com.god.life.repository.FcmAlarmRepository;
 import com.google.firebase.messaging.*;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class FcmAlarmService {
 
     private final FcmAlarmRepository fcmAlarmRepository;
-    private final FirebaseMessaging messaging;
+    private final FirebaseMessaging messenger;
 
 
     @Transactional(readOnly = true)
@@ -58,7 +63,7 @@ public class FcmAlarmService {
                         .addAllTokens(List.of(fcmSendDto.getToken()))
                         .build();
         try{
-            messaging.sendEachForMulticast(multicastMessage);
+            messenger.sendEachForMulticast(multicastMessage);
             return "TokenValue : " + sendToUser + "fcm 메세지 전송 성공!";
         } catch (FirebaseMessagingException e) {
             e.printStackTrace();
@@ -87,4 +92,55 @@ public class FcmAlarmService {
         //변경 감지
         fcmAlarm.updateAlarm(sendTime);
     }
+
+    private static final String END_MESSAGE_TITLE = "오늘 TODO 까먹지 않으셨죠?";
+    private static final String END_MESSAGE_BODY = "오늘 굿생기록을 정리해 보세요!";
+
+
+    // 1분마다
+    @Scheduled(cron = "0 * * * * *")
+    public void sendMessage() {
+        LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        List<String> sendToUserTokens = getUserTokenAtTime(now);
+
+        //보낼 대상이 없으면 알림 전송 XXX
+        if(sendToUserTokens == null || sendToUserTokens.size() == 0){
+            return;
+        }
+
+        log.info("이번에 보낼 사람 : {}", sendToUserTokens);
+
+
+        Notification notification = Notification
+                .builder()
+                .setTitle(END_MESSAGE_TITLE)
+                .setBody(END_MESSAGE_BODY)
+                .build();
+        MulticastMessage message = MulticastMessage
+                .builder()
+                .setNotification(notification)
+                .addAllTokens(sendToUserTokens)
+                .build();
+
+        try{
+            BatchResponse batchResponse = messenger.sendEachForMulticast(message);
+            int successCount = batchResponse.getSuccessCount();
+            if (successCount != sendToUserTokens.size()) {
+                log.error("FCM 전송 {} 명에게 전송 실패!", sendToUserTokens.size() - successCount);
+                List<SendResponse> responses = batchResponse.getResponses();
+                List<String> failTokens = new ArrayList<>(); //실패한 유저 ID LOG 저장
+                for (int i=0; i<responses.size(); i++) { //실패한 애들 찾아서 저장
+                    SendResponse response = responses.get(i);
+                    if(!response.isSuccessful()){
+                        failTokens.add(sendToUserTokens.get(i));
+                    }
+                }
+                log.error("전송 실패.. {}", failTokens);
+            }
+            log.info("알람 전송 성공!!!");
+        } catch (FirebaseMessagingException e) {
+            log.error("FCM 전송 오류!", e);
+        }
+    }
+
 }
