622992a787d8fdc889e81c76b8cfb12aa3f57436
HeoManBo <iovesssr__@naver.com>
2024-07-15 17:13:27 +0900

    Feat : 추천 작가, 게시물 선정, 삭제 기능 추가

--- src/main/java/com/god/life/admin/AdminController.java
+++ src/main/java/com/god/life/admin/AdminController.java
@@ -4,7 +4,7 @@ import com.god.life.dto.GodLifeStimulationBoardBriefResponse;
 import com.god.life.dto.ReportRequest;
 import com.god.life.dto.StimulationBoardSearchCondition;
 import com.god.life.service.BoardService;
-import com.god.life.service.MemberService;
+import com.god.life.service.RedisService;
 import com.god.life.service.ReportService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,10 +13,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Controller
@@ -27,8 +27,11 @@ public class AdminController {
 
     private final ReportService reportService;
     private final BoardService boardService;
-    private final MemberService memberService;
-    private final StimulationBoardSearchCondition emptyCondition = new StimulationBoardSearchCondition();
+    private final StimulationBoardSearchCondition EMPTY_CONDITION = new StimulationBoardSearchCondition();
+
+    private final RedisService redisService;
+    private final String RECOMMEND_BOARD_KEY = "board";
+    private final String RECOMMEND_AUTHOR_KEY = "author";
 
     @GetMapping("/main")
     public String mainPageView(
@@ -65,24 +68,82 @@ public class AdminController {
     @GetMapping("/recommend/board")
     public String selectBoardView(@RequestParam(value = "order", required = false) String orderType, Model model) {
         List<GodLifeStimulationBoardBriefResponse> dto =
-                boardService.getListStimulusBoardUsingSearchCondition(emptyCondition);
+                boardService.getListStimulusBoardUsingSearchCondition(EMPTY_CONDITION);
 
+        List<String> list = redisService.getList(RECOMMEND_BOARD_KEY);
+        List<GodLifeStimulationBoardBriefResponse> recommendList = new ArrayList<>();
+        for (int i = 0; i < dto.size(); i++) {
+            for (int j = 0; j < list.size(); j++) {
+                if (dto.get(i).getBoardId().equals(Long.valueOf(list.get(j)))) {
+                    recommendList.add(dto.get(i));
+                    dto.get(i).setView(-1);
+                    break;
+                }
+            }
+        }
+        log.info("recommend = {}", recommendList);
+        model.addAttribute("recommend", recommendList);
         model.addAttribute("boards", dto);
-        return "board";
+        return "boardList";
     }
 
     //추천 작가 선정
     @GetMapping("/recommend/author")
-    public String selectAuthor(Model model){
+    public String selectAuthor(Model model) {
         List<GodLifeStimulationBoardBriefResponse> dto
-                = boardService.getListStimulusBoardUsingSearchCondition(emptyCondition);
+                = boardService.getListStimulusBoardUsingSearchCondition(EMPTY_CONDITION);
 
+        // Nickname에 따라 그루핑
         Map<String, List<GodLifeStimulationBoardBriefResponse>> m =
                 dto.stream().collect(Collectors.groupingBy(GodLifeStimulationBoardBriefResponse::getNickname));
 
-        model.addAttribute("map", m);
+        String recommendAuthorNickname = redisService.getValues(RECOMMEND_AUTHOR_KEY);
+        if (!recommendAuthorNickname.equals(RedisService.NO_VALUE)) {
+            Set<String> keys = m.keySet();
+            for (String key : keys) {
+                if (key.equals(recommendAuthorNickname)) {
+                    model.addAttribute("recommendAuthor", m.get(recommendAuthorNickname));
+                    m = dto.stream().filter(d -> !d.getNickname().equals(recommendAuthorNickname))
+                            .collect(Collectors.groupingBy(GodLifeStimulationBoardBriefResponse::getNickname));
+                }
+            }
+        }
 
+        model.addAttribute("map", m);
         return "author";
+    }
+
+    // boardId에 해당하는 게시물을 추천 갓생 자극 게시물로 선정
+    @PostMapping("/recommend/board/{boardId}")
+    public String selectRecommendBoard(@PathVariable("boardId") String id) {
+        redisService.addValueToRightOnList(RECOMMEND_BOARD_KEY, id);
+
+        return "redirect:/admin/recommend/board";
+    }
+
+    // memberId에 해당하는 회원을 추천 작가로 선정
+    @PostMapping("/recommend/author/{nickname}")
+    public String selectRecommendAuthor(@PathVariable("nickname") String nickname) {
+        redisService.setValue(RECOMMEND_AUTHOR_KEY, nickname);
+        return "redirect:/admin/recommend/author";
+    }
+
+    //추천 작가 취소
+    @PostMapping("/recommend/author")
+    public String deleteRecommendAuthor() {
+        redisService.deleteValue(RECOMMEND_AUTHOR_KEY);
+        return "redirect:/admin/recommend/author";
+    }
+
+    @PostMapping("/recommend/board")
+    public String deleteRecommendBoard(@ModelAttribute(name = "id") String removeId){
+        redisService.deleteValueInList(RECOMMEND_BOARD_KEY, removeId);
+        return "redirect:/admin/recommend/board";
+    }
+
+    @ExceptionHandler(value = Exception.class)
+    public void ex(Exception exception) {
+        log.info("", exception);
     }
 
 }
